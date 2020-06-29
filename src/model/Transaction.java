0810d174529d14bbf8bb405a4b1ecb7f5e14afa4
Joanna <>
2020-06-29 19:39:56 +0700

    Revisi Transaksi, Hotel, Room

--- src/model/Transaction.java
+++ src/model/Transaction.java
@@ -11,16 +11,24 @@ import java.util.Date;
  *
  * @author 1119034 Eirenika Joanna Grace Lendeng
  */
-public class Transaksi {
+public class Transaction {
     private int idTransaksi;
+    private int idHotel;
+    private int idJenisPembayaran;
+    private String idUser;
+    private String noKamar;
     private int jumlahGuest;
     private int uangMuka;
     private Date tanggalCheckIn;
     private Date tanggalCheckOut;
     private Date tanggalBooking;
 
-    public Transaksi(int idTransaksi, int jumlahGuest, int uangMuka, Date tanggalCheckIn, Date tanggalCheckOut, Date tanggalBooking) {
+    public Transaksi(int idTransaksi, int idHotel, int idJenisPembayaran, String idUser, String noKamar, int jumlahGuest, int uangMuka, Date tanggalCheckIn, Date tanggalCheckOut, Date tanggalBooking) {
         this.idTransaksi = idTransaksi;
+        this.idHotel = idHotel;
+        this.idJenisPembayaran = idJenisPembayaran;
+        this.idUser = idUser;
+        this.noKamar = noKamar;
         this.jumlahGuest = jumlahGuest;
         this.uangMuka = uangMuka;
         this.tanggalCheckIn = tanggalCheckIn;
@@ -34,6 +42,38 @@ public class Transaksi {
 
     public void setIdTransaksi(int idTransaksi) {
         this.idTransaksi = idTransaksi;
+    }
+
+    public int getIdHotel() {
+        return idHotel;
+    }
+
+    public void setIdHotel(int idHotel) {
+        this.idHotel = idHotel;
+    }
+
+    public int getIdJenisPembayaran() {
+        return idJenisPembayaran;
+    }
+
+    public void setIdJenisPembayaran(int idJenisPembayaran) {
+        this.idJenisPembayaran = idJenisPembayaran;
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getNoKamar() {
+        return noKamar;
+    }
+
+    public void setNoKamar(String noKamar) {
+        this.noKamar = noKamar;
     }
 
     public int getJumlahGuest() {
@@ -75,6 +115,6 @@ public class Transaksi {
     public void setTanggalBooking(Date tanggalBooking) {
         this.tanggalBooking = tanggalBooking;
     }
-    
+
     
 }
